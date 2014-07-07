0bf27c1301e83cad650108c99f91ec76100c2509
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 09:00:23 -0700

    downcase word before capitalizing it

--- lib/title_case.rb
+++ lib/title_case.rb
@@ -1,4 +1,5 @@
 def title_case(text)
+  text = text.downcase
   text[0] = text[0].upcase
   text
 end
--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -2,8 +2,11 @@ require('rspec')
 require('title_case')
 
 describe('title_case') do
-  it('capitalizes the initial letter of a word') do
+  it('capitalizes the initial letter of a word in lowercase') do
     expect(title_case('hello')).to eq('Hello')
+  end
+  it('lowercases a word in all upper case before capitalizing') do
+    expect(title_case('HELLO')).to eq('Hello')
   end
 end
 
