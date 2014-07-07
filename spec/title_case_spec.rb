b15f9ed3adf59420e9210a03b28e675debd09ee8
Willem Larsen <willemlarsen@gmail.com>
2014-07-07 09:02:55 -0700

    pass test for handling words in mixed cases

--- spec/title_case_spec.rb
+++ spec/title_case_spec.rb
@@ -8,5 +8,8 @@ describe('title_case') do
   it('lowercases a word in all upper case before capitalizing') do
     expect(title_case('HELLO')).to eq('Hello')
   end
+  it('lowercases a word in mixed cases before capitalizing it') do
+    expect(title_case("HeLlO")).to eq('Hello')
+  end
 end
 
